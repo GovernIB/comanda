9c514b7229097fabaf0886094f24b43377140caa
Josep Gayà <josepg@limit.es>
2025-01-28 07:34:54 +0100

    503 - Implementació del MS de Salut

--- comanda-ms-configuracio/src/main/java/es/caib/comanda/configuracio/ComandaConfiguracioApp.java
+++ comanda-ms-configuracio/src/main/java/es/caib/comanda/configuracio/ComandaConfiguracioApp.java
@@ -3,8 +3,6 @@ package es.caib.comanda.configuracio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.hateoas.config.EnableHypermediaSupport;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Classe principal del microservei comanda-configuracio.
