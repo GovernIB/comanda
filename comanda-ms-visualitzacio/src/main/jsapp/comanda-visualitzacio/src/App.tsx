30a083157e6df4bdff476458b305dfa08c9ee162
Josep Gayà <josepg@limit.es>
2025-08-20 09:20:36 +0200

    Reorganització menús

--- comanda-ms-visualitzacio/src/main/jsapp/comanda-visualitzacio/src/App.tsx
+++ comanda-ms-visualitzacio/src/main/jsapp/comanda-visualitzacio/src/App.tsx
@@ -14,47 +14,54 @@ export const App: React.FC = () => {
         icon: 'monitor_heart',
         resourceName: 'salut',
     };
-    const menuEstadistiquesReduit = {
+    const menuEstadistiques = {
         id: 'estadistiques',
         title: t('menu.estadistiques'),
         to: '/estadistiques',
         icon: 'bar_chart',
         resourceName: 'dashboard',
     }
-    const menuEstadistiques = {
-        id: 'estadistiques',
-        title: t('menu.estadistiques'),
-        icon: 'bar_chart',
+    const menuTasca = {
+        id: 'tasca',
+        title: t('menu.tasca'),
+        to: '/tasca',
+        icon: 'assignment',
+        resourceName: 'tasca',
+    }
+    const menuAvis = {
+        id: 'avis',
+        title: t('menu.avis'),
+        to: '/avis',
+        icon: 'warning',
+        resourceName: 'avis',
+    }
+    const menuMonitoritzacio = {
+        id: 'monitoritzacio',
+        title: t('menu.monitoritzacio'),
+        icon: 'settings',
         children: [
             {
-                id: 'estadistiques',
-                title: t('menu.estadistiques'),
-                to: '/estadistiques',
-                icon: 'bar_chart',
-                resourceName: 'dashboard',
+                id: 'monitor',
+                title: t('menu.monitor'),
+                to: '/monitor',
+                icon: 'monitor',
+                resourceName: 'monitor',
             },
             {
-                id: 'estadisticaWidget',
-                title: t('menu.widget'),
-                to: '/estadisticaWidget',
-                icon: 'widgets',
-                resourceName: 'dashboard',
+                id: 'cache',
+                title: t('menu.cache'),
+                to: '/cache',
+                icon: 'storage',
+                resourceName: 'comandaCache',
             },
             {
-                id: 'dashboard',
-                title: t('menu.dashboard'),
-                to: '/dashboard',
-                icon: 'dashboardCustomize',
-                resourceName: 'dashboard',
-            },
-            {
-                id: 'calendari',
-                title: t('menu.calendari'),
-                to: '/calendari',
-                icon: 'calendar_month',
-                resourceName: 'fet',
-            },
-        ],
+                id: 'broker',
+                title: t('menu.broker'),
+                to: '/broker',
+                icon: 'send_time_extension',
+                resourceName: 'broker',
+            }
+        ]
     };
     const menuConfiguracio = {
         id: 'configuracio',
@@ -89,64 +96,49 @@ export const App: React.FC = () => {
                 icon: 'integration_instructions',
                 resourceName: 'integracio',
             },
+            {
+                id: 'estadisticaWidget',
+                title: t('menu.widget'),
+                to: '/estadisticaWidget',
+                icon: 'widgets',
+                resourceName: 'dashboard',
+            },
+            {
+                id: 'dashboard',
+                title: t('menu.dashboard'),
+                to: '/dashboard',
+                icon: 'dashboardCustomize',
+                resourceName: 'dashboard',
+            },
+            {
+                id: 'calendari',
+                title: t('menu.calendari'),
+                to: '/calendari',
+                icon: 'calendar_month',
+                resourceName: 'fet',
+            },
+            {
+                id: 'parametre',
+                title: t('menu.parametre'),
+                to: '/parametre',
+                icon: 'settings',
+                resourceName: 'parametre',
+            }
         ]
     };
-    const menuMonitor = {
-        id: 'monitor',
-        title: t('menu.monitor'),
-        to: '/monitor',
-        icon: 'monitor',
-        resourceName: 'monitor',
-    }
-    const menuCache = {
-        id: 'cache',
-        title: t('menu.cache'),
-        to: '/cache',
-        icon: 'storage',
-        resourceName: 'comandaCache',
-    }
-    const menuParametre = {
-        id: 'parametre',
-        title: t('menu.parametre'),
-        to: '/parametre',
-        icon: 'settings',
-        resourceName: 'parametre',
-    }
-    const menuBroker = {
-        id: 'broker',
-        title: t('menu.broker'),
-        to: '/broker',
-        icon: 'send_time_extension',
-        resourceName: 'broker',
-    }
-    const menuTasca = {
-        id: 'tasca',
-        title: t('menu.tasca'),
-        to: '/tasca',
-        icon: 'assignment',
-        resourceName: 'tasca',
-    }
-    const menuAvis = {
-        id: 'avis',
-        title: t('menu.avis'),
-        to: '/avis',
-        icon: 'warning',
-        resourceName: 'avis',
-    }
     const menuEntries = [
         menuSalut,
         menuEstadistiques,
-        menuConfiguracio,
-        menuMonitor,
-        menuParametre,
-        menuCache,
-        menuBroker,
         menuTasca,
-        menuAvis
+        menuAvis,
+        menuMonitoritzacio,
+        menuConfiguracio,
     ];
     const appMenuEntries = [
         menuSalut,
-        menuEstadistiquesReduit
+        menuEstadistiques,
+        menuTasca,
+        menuAvis,
     ];
     return (
         <BaseApp
