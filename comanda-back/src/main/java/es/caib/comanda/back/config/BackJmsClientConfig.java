9a8a05b834294e539352d1629a31d2d89fcd454f
Josep Gayà <josepg@limit.es>
2025-08-28 08:42:31 +0200

    Afegir diferents ROLS a l'aplicació

--- comanda-back/src/main/java/es/caib/comanda/back/config/BackJmsClientConfig.java
+++ comanda-back/src/main/java/es/caib/comanda/back/config/BackJmsClientConfig.java
@@ -5,7 +5,6 @@ import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
