8a027616df3df74bcde4dff638bb889de9bcfd61
Josep Gayà <josepg@limit.es>
2025-08-29 14:10:47 +0200

    Afegir broker de missatges per que les aplicacions puguin enviar informació asíncronament

--- comanda-api-interna/src/main/java/es/caib/comanda/api/controller/JmsRestController.java
+++ comanda-api-interna/src/main/java/es/caib/comanda/api/controller/JmsRestController.java
@@ -5,6 +5,7 @@ import es.caib.comanda.ms.back.controller.BaseController;
 import es.caib.comanda.ms.broker.model.Avis;
 import es.caib.comanda.ms.broker.model.Permis;
 import es.caib.comanda.ms.broker.model.Tasca;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
@@ -27,41 +28,45 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequiredArgsConstructor
 public class JmsRestController extends BaseController {
 
-    private final JmsTemplate jmsTemplate;
+	private final JmsTemplate jmsTemplate;
 
-    @GetMapping
-    public ResponseEntity<CollectionModel<?>> index() {
-        List<Link> indexLinks = new ArrayList<>();
-        indexLinks.add(linkTo(methodOn(getClass()).sendToTasquesQueue(null)).withRel("tasques"));
-        indexLinks.add(linkTo(methodOn(getClass()).sendToAvisosQueue(null)).withRel("avisos"));
-        indexLinks.add(linkTo(methodOn(getClass()).sendToPermisosQueue(null)).withRel("permisos"));
-        CollectionModel<?> resources = CollectionModel.of(
-                Collections.emptySet(),
-                indexLinks.toArray(Link[]::new));
-        return ResponseEntity.ok(resources);
-    }
+	@GetMapping
+	@Operation(summary = "Llista d'opcions disponibles")
+	public ResponseEntity<CollectionModel<?>> index() {
+		List<Link> indexLinks = new ArrayList<>();
+		indexLinks.add(linkTo(methodOn(getClass()).sendToTasquesQueue(null)).withRel("tasques"));
+		indexLinks.add(linkTo(methodOn(getClass()).sendToAvisosQueue(null)).withRel("avisos"));
+		indexLinks.add(linkTo(methodOn(getClass()).sendToPermisosQueue(null)).withRel("permisos"));
+		CollectionModel<?> resources = CollectionModel.of(
+				Collections.emptySet(),
+				indexLinks.toArray(Link[]::new));
+		return ResponseEntity.ok(resources);
+	}
 
-    @PostMapping("/tasques")
-    public ResponseEntity<String> sendToTasquesQueue(@RequestBody Tasca tasca) {
-        jmsTemplate.convertAndSend(CUA_TASQUES, tasca);
-        return ResponseEntity.ok("Missatge enviat a " + CUA_TASQUES);
-    }
+	@PostMapping("/tasques")
+	@Operation(summary = "Afegeix un missatge a la cua de tasques")
+	public ResponseEntity<String> sendToTasquesQueue(@RequestBody Tasca tasca) {
+		jmsTemplate.convertAndSend(CUA_TASQUES, tasca);
+		return ResponseEntity.ok("Missatge enviat a " + CUA_TASQUES);
+	}
 
-    @PostMapping("/avisos")
-    public ResponseEntity<String> sendToAvisosQueue(@RequestBody Avis avis) {
-        jmsTemplate.convertAndSend(CUA_AVISOS, avis);
-        return ResponseEntity.ok("Missatge enviat a " + CUA_AVISOS);
-    }
+	@PostMapping("/avisos")
+	@Operation(summary = "Afegeix un missatge a la cua d'avisos")
+	public ResponseEntity<String> sendToAvisosQueue(@RequestBody Avis avis) {
+		jmsTemplate.convertAndSend(CUA_AVISOS, avis);
+		return ResponseEntity.ok("Missatge enviat a " + CUA_AVISOS);
+	}
 
-    @PostMapping("/permisos")
-    public ResponseEntity<String> sendToPermisosQueue(@RequestBody Permis permis) {
-        jmsTemplate.convertAndSend(CUA_PERMISOS, permis);
-        return ResponseEntity.ok("Missatge enviat a " + CUA_PERMISOS);
-    }
+	@PostMapping("/permisos")
+	@Operation(summary = "Afegeix un missatge a la cua de permisos")
+	public ResponseEntity<String> sendToPermisosQueue(@RequestBody Permis permis) {
+		jmsTemplate.convertAndSend(CUA_PERMISOS, permis);
+		return ResponseEntity.ok("Missatge enviat a " + CUA_PERMISOS);
+	}
 
-    @Override
-    protected Link getIndexLink() {
-        return linkTo(methodOn(getClass()).index()).withRel("jms");
-    }
+	@Override
+	protected Link getIndexLink() {
+		return linkTo(methodOn(getClass()).index()).withRel("jms");
+	}
 
 }
