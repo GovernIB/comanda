160604fa3743ab4e689a773a7f9157b5a2bfde6a
josepg-limit <josepg@limit.es>
2025-10-27 19:14:41 +0100

    Revisió treeData.tsx i tipar les variables d'entorn usades

--- comanda-ms-visualitzacio/src/main/jsapp/comanda-visualitzacio/src/vite-env.d.ts
+++ comanda-ms-visualitzacio/src/main/jsapp/comanda-visualitzacio/src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+    readonly VITE_API_PUBLIC_URL: string;
+    readonly VITE_API_BASE_URL: string;
+    readonly VITE_API_SUFFIX: string;
+    readonly VITE_AUTH_PROVIDER_URL: string;
+    readonly VITE_AUTH_PROVIDER_REALM: string;
+    readonly VITE_AUTH_PROVIDER_CLIENTID: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
