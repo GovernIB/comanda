96750938d78f5f0dc6bdf74941e42b96fcdead1e
josepg-limit <josepg@limit.es>
2026-03-16 14:22:15 +0100

    Comentats tests d’integració d'exemple AbstractBaseIntegrationTest i H2IntegrationTest.

--- comanda-ms-configuracio/src/test/java/es/caib/comanda/configuracio/integration/AbstractBaseIntegrationTest.java
+++ comanda-ms-configuracio/src/test/java/es/caib/comanda/configuracio/integration/AbstractBaseIntegrationTest.java
@@ -13,29 +13,29 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers(disabledWithoutDocker = true)
-@ActiveProfiles("test")
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@Testcontainers(disabledWithoutDocker = true)
+//@ActiveProfiles("test")
 public class AbstractBaseIntegrationTest {
 
-    @MockBean
-    JwtDecoder jwtDecoder;
-
-    @Container
-    static OracleContainer oracle = new OracleContainer("gvenzl/oracle-free:slim-faststart")
-            .withDatabaseName("testdb")
-            .withUsername("testuser")
-            .withPassword("testpass");
-
-    @DynamicPropertySource
-    static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", oracle::getJdbcUrl);
-        registry.add("spring.datasource.username", oracle::getUsername);
-        registry.add("spring.datasource.password", oracle::getPassword);
-    }
-
-    @Test
-    void containerIsRunning() {
-        assertTrue(oracle.isRunning());
-    }
+//    @MockBean
+//    JwtDecoder jwtDecoder;
+//
+//    @Container
+//    static OracleContainer oracle = new OracleContainer("gvenzl/oracle-free:slim-faststart")
+//            .withDatabaseName("testdb")
+//            .withUsername("testuser")
+//            .withPassword("testpass");
+//
+//    @DynamicPropertySource
+//    static void properties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", oracle::getJdbcUrl);
+//        registry.add("spring.datasource.username", oracle::getUsername);
+//        registry.add("spring.datasource.password", oracle::getPassword);
+//    }
+//
+//    @Test
+//    void containerIsRunning() {
+//        assertTrue(oracle.isRunning());
+//    }
 }
--- comanda-ms-configuracio/src/test/java/es/caib/comanda/configuracio/integration/H2IntegrationTest.java
+++ comanda-ms-configuracio/src/test/java/es/caib/comanda/configuracio/integration/H2IntegrationTest.java
@@ -14,27 +14,27 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test-h2")
-@TestPropertySource(properties = {
-        "es.caib.comanda.properties=classpath:application-test-h2.properties",
-        "es.caib.comanda.system.properties=classpath:application-test-h2.properties"
-})
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@ActiveProfiles("test-h2")
+//@TestPropertySource(properties = {
+//        "es.caib.comanda.properties=classpath:application-test-h2.properties",
+//        "es.caib.comanda.system.properties=classpath:application-test-h2.properties"
+//})
 public class H2IntegrationTest {
 
-    @MockBean
-    JwtDecoder jwtDecoder;
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Test
-    void testH2Connection() throws SQLException {
-        assertNotNull(dataSource);
-        try (Connection connection = dataSource.getConnection()) {
-            assertNotNull(connection);
-            String url = connection.getMetaData().getURL();
-            assertTrue(url.contains("jdbc:h2:mem:testdb"), "URL should be H2 mem: " + url);
-        }
-    }
+//    @MockBean
+//    JwtDecoder jwtDecoder;
+//
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    @Test
+//    void testH2Connection() throws SQLException {
+//        assertNotNull(dataSource);
+//        try (Connection connection = dataSource.getConnection()) {
+//            assertNotNull(connection);
+//            String url = connection.getMetaData().getURL();
+//            assertTrue(url.contains("jdbc:h2:mem:testdb"), "URL should be H2 mem: " + url);
+//        }
+//    }
 }
